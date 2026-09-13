79e72fe98bd354759bdf44f5e9f9561ed59dc9c8
almeida2 <projetodidaticovl@gmail.com>
2026-09-13 19:23:55 -0300

    feat: versão inicial

---
+++ src/main/java/com/fatec/cap05ldw/Cap06tddApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.cap05ldw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cap06tddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cap06tddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/cap05ldw/Cap06tddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.cap05ldw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cap06tddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
