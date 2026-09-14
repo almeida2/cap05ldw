874c9677bde8ae3f16a078e4f75fcca43abdec18
almeida2 <projetodidaticovl@gmail.com>
2026-09-14 11:08:14 -0300

    test: falha gerada na configuracao do contexto

--- src/test/java/com/fatec/cap05ldw/Cap06tddApplicationTests.java
+++ src/test/java/com/fatec/cap05ldw/Cap06tddApplicationTests.java
@@ -1,5 +1,7 @@
 package com.fatec.cap05ldw;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class Cap06tddApplicationTests {
 
 	@Test
 	void contextLoads() {
+		fail("Falha intencional na configuracao do contexto");
 	}
 
 }
